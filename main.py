ba8adfedeeaa5872616512907313677a3102bfd9
Muhammadbilol Qo'chqorov <mblabx@gmail.com>
2026-06-20 13:10:54 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+import os
+import requests
+from aiogram import Bot, Dispatcher, types
+from aiogram.types import InputFile
+from aiogram.utils import executor
+from api import download_instagram_video
+
+API_TOKEN = '7044287189:AAF9uXbbz6A3Pq_cG1iHQ6FB6J9DoxkrHVc'
+
+# Bot va dispatcher obyektlarini yaratish
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+# /start komandasi uchun handler
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    await message.reply("Salom! Instagram videolarini yuklash uchun URL yuboring.")
+
+
+# URL uchun handler
+@dp.message_handler()
+async def fetch_video(message: types.Message):
+    url = message.text
+    await message.reply("Videoni yuklamoqdamiz, biroz kuting...")
+
+    video_file = download_instagram_video(url)
+    if video_file:
+        await message.reply_video(video=InputFile(video_file))
+        os.remove(video_file)  # Faylni o'chirib tashlash
+    else:
+        await message.reply("Video yuklashda xato yuz berdi.")
+
+
+# Xatolarni qayd qilish
+@dp.errors_handler()
+async def error_handler(update, error):
+    print(f"Update: {update} caused error: {error}")
+    return True
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
